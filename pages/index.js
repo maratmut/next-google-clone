c1999abe80a19986da4dc7db8f85564136f532b0
Marat Mutalipov <maratios41@gmail.com>
2023-02-18 19:58:04 +0300

    Create the Header section

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
-import Head from 'next/head'
-
+import Header from '@/components/Header';
+import Head from 'next/head';
 
 export default function Home() {
   return (
@@ -10,7 +10,11 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-        <h1>Hello world!..</h1>
+      {/*Header*/}
+      <Header />
+      {/*Body*/}
+
+      {/*Footer*/}
     </>
-  )
+  );
 }
