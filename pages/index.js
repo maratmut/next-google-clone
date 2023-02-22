beebadd01488376d9978907b0d70547f44a47fae
Marat Mutalipov <maratios41@gmail.com>
2023-02-22 20:14:08 +0300

    Get the term from input and redirect to the related page

--- pages/index.js
+++ pages/index.js
@@ -3,8 +3,20 @@ import Head from 'next/head';
 import Image from 'next/image';
 import { SearchIcon, MicrophoneIcon } from '@heroicons/react/solid';
 import Footer from '@/components/Footer';
+import { useRouter } from 'next/router';
+import { useRef } from 'react';
 
 export default function Home() {
+  const router = useRouter()
+  const searchInputRef = useRef(null)
+
+  function search(e) {
+    e.preventDefault()
+    const term = searchInputRef.current.value
+    if(!term.trim()) return
+    router.push(`/search?term=${term.trim()}`)
+  }
+
   return (
     <>
       <Head>
@@ -25,12 +37,12 @@ export default function Home() {
         />
         <div className="flex items-center p-2 mt-5 hover:shadow-inner justify-between rounded-full w-full py-3 px-5 mx-auto max-w-[90%] sm:max-w-xl border border-gray-200">
           <SearchIcon className="h-5 text-gray-500 mr-2 " />
-          <input className="w-[93%] outline-0 " type="text" />
+          <input ref={searchInputRef} className="w-[93%] outline-0 " type="text" />
           <MicrophoneIcon className="h-5" />
         </div>
 
         <div className="flex flex-col sm:flex-row w-[50%] space-y-2 mt-8 sm:space-y-0 sm:space-x-4 justify-center">
-          <button className="btn">Google Search</button>
+          <button onClick={search} className="btn">Google Search</button>
           <button className="btn">I'm Feeling Lucky</button>
         </div>
       </form>
